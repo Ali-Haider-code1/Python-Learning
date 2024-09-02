22727664c6960a67310d94a4933d3356344ce5ea
Ali-Haider-code1 <AliHaiderAwan1345@gmail.com>
2024-09-02 12:43:03 +0500

    Update function implemented.

--- crud.py
+++ crud.py
@@ -10,9 +10,20 @@ def write_user():
     for user in users_data:
         print(user)
 
-
-
+def update_user(index,new_record):
+    if 0 <= index < len(users_data):
+        print(f"Updating record at index {index} from {users_data[index]} to {new_record}")
+        users_data[index] = new_record
+    else:
+        print("Invalid index")
+        
+    
+    
 create_user({'id':1,'name':'Ali Haider','age':22})
 
 write_user()
+update_user(0, {'id': 2, 'name': 'Ali','age':21})
+print('After Updating the data')
+write_user()
+
     
