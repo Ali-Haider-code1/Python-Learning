d178fefecf771ce89d02a1f0747ec538bee9857e
Ali-Haider-code1 <AliHaiderAwan1345@gmail.com>
2024-09-03 11:12:38 +0500

    Flask API Created using FLASK Library.

--- crud.py
+++ crud.py
@@ -1,32 +1,31 @@
 
-users_data = []
+# users_data = []
 
-def create_user(data):
-    users_data.append(data)
-    print('user data added successfully!')
+# def create_user(data):
+#     users_data.append(data)
+#     print('user data added successfully!')
 
 
-def write_user():
-    for user in users_data:
-        print(user)
+# def write_user():
+#     for user in users_data:
+#         print(user)
 
-def update_user(index,new_record):
-    if 0 <= index < len(users_data):
-        print(f"Updating record at index {index} from {users_data[index]} to {new_record}")
-        users_data[index] = new_record
-    else:
-        print("Invalid index")
+# def update_user(index,new_record):
+#     if 0 <= index < len(users_data):
+#         print(f"Updating record at index {index} from {users_data[index]} to {new_record}")
+#         users_data[index] = new_record
+#     else:
+#         print("Invalid index")
         
-def delete_user(index):
-    if 0 <= index < len(users_data):
-        print('Deleting data...')
-        del users_data(index)
+# def delete_user(index):
+#     if 0 <= index < len(users_data):
+#         print('Deleting data...')
+#         del users_data[index]
     
-create_user({'id':1,'name':'Ali Haider','age':22})
+# create_user({'id':1,'name':'Ali Haider','age':22})
 
-write_user()
-update_user(0, {'id': 2, 'name': 'Ali','age':21})
-print('After Updating the data')
-write_user()
+# write_user()
+# update_user(0, {'id': 2, 'name': 'Ali','age':21})
+# print('After Updating the data')
+# write_user()
 
-    
